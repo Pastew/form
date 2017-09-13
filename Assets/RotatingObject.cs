fd5b25d75ad0fa0c178592892fb5cd542eedf52e
Pastew <kpastew@gmail.com>
2017-09-14 01:08:01 +0200

    Stupid fans riddle

---
+++ Assets/RotatingObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour {
+
+    [Tooltip("Degrees per second")]
+    public float rotateSpeed = 10;
+
+	void Update () {
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+	}
+}
