76abf1bfc9307c21f2557e0d5663d6328957c8ed
Pastew <kpastew@gmail.com>
2017-09-14 20:03:59 +0200

    Fixed bug when player dies before any checkpoint

--- Assets/Checkpoints.cs
+++ Assets/Checkpoints.cs
@@ -6,11 +6,14 @@ using System.Linq;
 
 public class Checkpoints : MonoBehaviour {
 
+    private Vector3 playerStartingPosition;
+
     Checkpoint[] checkpoints;
     int currentCheckpoint = 0;
 
 	void Start () {
         checkpoints = GetComponentsInChildren<Checkpoint>();
+        playerStartingPosition = FindObjectOfType<Player>().transform.position;
     }
 
     internal void ActivateNextCheckpoint()
@@ -22,7 +25,15 @@ public class Checkpoints : MonoBehaviour {
 
     public void ResetToLastCheckpoint()
     {
-        print("Dead. I will move player to checkpoint: " + (currentCheckpoint-1));
-        FindObjectOfType<Player>().TeleportToPosition(checkpoints[currentCheckpoint-1].transform.position);
+        if (currentCheckpoint > 0)
+        {
+            print("Dead. I will move player to checkpoint: " + (currentCheckpoint - 1));
+            FindObjectOfType<Player>().TeleportToPosition(checkpoints[currentCheckpoint - 1].transform.position);
+        }
+        else
+        {
+            print("Dead. I will move player to starting position");
+            FindObjectOfType<Player>().TeleportToPosition(playerStartingPosition);
+        }
     }
 }
