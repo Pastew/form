0729b4e32729fdab1668eef28a5b7c5a0b398b8a
Pastew <kpastew@gmail.com>
2017-10-10 21:51:49 +0200

    Stub InGameAd

---
+++ Assets/InGameAd.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameAd : MonoBehaviour {
+
+	void Start () {
+        print("hehe");
+        gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("InGameAds/StubAd");
+    }
+	
+	void Update () {
+		
+	}
+}
